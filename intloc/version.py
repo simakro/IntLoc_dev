867e697abb526d1d99938d62e56a8b50ce5cd0ea
simakro <74452164+simakro@users.noreply.github.com>
2024-07-03 08:12:53 +0200

    prepped preview branch

--- intloc/version.py
+++ intloc/version.py
@@ -2,4 +2,4 @@
 # Licensed under the BSD-2-clause License (https://opensource.org/licenses/BSD-2-Clause).
 # This file may not be copied, modified, or distributed except according to those terms.
 
-__version__ = "0.0.dev155"
+__version__ = "0.0.dev_preview" # This is an incomplete developmental branch.
